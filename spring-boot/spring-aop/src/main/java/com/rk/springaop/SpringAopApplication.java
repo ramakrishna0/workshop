6991e6da49dec51b2a5685f90558de54bbed68af
Rama Krishna <ramakrishnab.1508@gmail.com>
2023-05-26 03:13:38 -0700

    Autowired using lombok annotations

--- spring-boot/spring-aop/src/main/java/com/rk/springaop/SpringAopApplication.java
+++ spring-boot/spring-aop/src/main/java/com/rk/springaop/SpringAopApplication.java
@@ -1,5 +1,9 @@
 package com.rk.springaop;
 
+import com.rk.springaop.business.Business1;
+import com.rk.springaop.business.Business2;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,18 +11,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.rk.springaop.business.Business1;
-import com.rk.springaop.business.Business2;
-
 @SpringBootApplication
+@AllArgsConstructor 		//(onConstructor = @__(@Autowired)) --> using in spring version < 4.3
 public class SpringAopApplication implements CommandLineRunner {
 
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
-
-	@Autowired
-	private Business1 business1;
-	@Autowired
-	private Business2 business2;
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	// all variables are autowired automatically using @AllArgsConstructor or @RequiredArgsConstructor
+	//Even though it is not listed, this annotation also has the onConstructor parameter.
+	//Autowired variables should be unambiguous
+//	@Autowired
+	private final Business1 business1;
+//	@Autowired
+	private final Business2 business2;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAopApplication.class, args);
