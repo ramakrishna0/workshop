3420b0dccac985feabfd9e54be777de7686a8483
Rama Krishna <ramakrishnab.1508@gmail.com>
2023-05-27 04:09:22 -0700

    streams

--- Maven/src/main/java/com/java/streams/ParallelStreams.java
+++ Maven/src/main/java/com/java/streams/ParallelStreams.java
@@ -9,14 +9,13 @@ public class ParallelStreams {
         Stream<String> stream1 = Stream.of("sheep", "goat", "horse").parallel();
 //        Stream<String> stream2 = List.of("sheep", "goat", "horse").stream().parallel();
         Stream<String> stream2 = List.of("sheep", "goat", "horse").parallelStream();
+        stream1.forEach(System.out::println);
+        stream2.forEach(System.out::println);
 
         int sum = Stream.of(10, 20, 30, 40, 50)
                 .parallel()
                 .mapToInt(Integer::intValue)
                 .sum();
         System.out.println("sum: " + sum);
-        Stream.of(10, 20, 30, 40, 50)
-                .parallel()
-                .forEach(System.out::println);
     }
 }
