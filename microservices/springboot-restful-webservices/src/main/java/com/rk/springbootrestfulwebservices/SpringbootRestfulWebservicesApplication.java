1061cdfd2485afcf322f6ff32a86fa20ccac3d7a
Rama Krishna <ramakrishnab.1508@gmail.com>
2023-07-17 16:34:51 -0500

    springboot-restful-webservices

---
+++ microservices/springboot-restful-webservices/src/main/java/com/rk/springbootrestfulwebservices/SpringbootRestfulWebservicesApplication.java
@@ -0,0 +1,15 @@
+package com.rk.springbootrestfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfulWebservicesApplication.class, args);
+
+
+	}
+
+}
