562f69469c14b319ea7e34f598c841e80c073478
Rama Krishna <ramakrishnab.1508@gmail.com>
2023-07-21 01:56:02 -0500

    Model Mapper

--- microservices/springboot-restful-webservices/src/main/java/com/rk/springbootrestfulwebservices/dto/UserDto.java
+++ microservices/springboot-restful-webservices/src/main/java/com/rk/springbootrestfulwebservices/dto/UserDto.java
@@ -2,9 +2,10 @@ package com.rk.springbootrestfulwebservices.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
+@AllArgsConstructor @NoArgsConstructor
 @Getter @Setter
 public class UserDto {
     private Long id;
--- microservices/springboot-restful-webservices/src/main/java/com/rk/springbootrestfulwebservices/service/impl/UserServiceImpl.java
+++ microservices/springboot-restful-webservices/src/main/java/com/rk/springbootrestfulwebservices/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.rk.springbootrestfulwebservices.mapper.UserMapper;
 import com.rk.springbootrestfulwebservices.repository.UserRepository;
 import com.rk.springbootrestfulwebservices.service.UserService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,28 +15,35 @@ import java.util.Optional;
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
-//    @Autowired //can be omitted for single instance variable class
+//    @Autowired //can be omitted for classes with one constructor
     private UserRepository userRepository;
+    private ModelMapper modelMapper;
     @Override
     public UserDto createUser(UserDto userDto) {
         //convert userDto to user
-        User user = UserMapper.mapToUser(userDto);
+//        User user = UserMapper.mapToUser(userDto);
+        User user = modelMapper.map(userDto, User.class);
         User savedUser = userRepository.save(user);
         // convert user to userDto
-        return UserMapper.mapToUserDto(savedUser);
+//        return UserMapper.mapToUserDto(savedUser);
+        return modelMapper.map(savedUser, UserDto.class);
     }
 
     @Override
     public UserDto getUserById(Long userId) {
         Optional<User> optionalUser = userRepository.findById(userId);
 //        return optionalUser.get();
-        return UserMapper.mapToUserDto(optionalUser.get());
+//        return UserMapper.mapToUserDto(optionalUser.get());
+        return modelMapper.map(optionalUser.get(), UserDto.class);
     }
 
     @Override
     public List<UserDto> getAllUsers() {
-        return userRepository.findAll().stream()
+        /*return userRepository.findAll().stream()
                 .map(UserMapper::mapToUserDto)
+                .toList();*/
+        return userRepository.findAll().stream()
+                .map(user -> modelMapper.map(user, UserDto.class))
                 .toList();
     }
 
@@ -45,7 +53,8 @@ public class UserServiceImpl implements UserService {
         existingUser.setFirstName(user.getFirstName());
         existingUser.setLastName(user.getLastName());
         existingUser.setEmail(user.getEmail());
-        return UserMapper.mapToUserDto(userRepository.save(existingUser));
+//        return UserMapper.mapToUserDto(userRepository.save(existingUser));
+        return modelMapper.map(userRepository.save(existingUser), UserDto.class);
     }
 
     @Override
