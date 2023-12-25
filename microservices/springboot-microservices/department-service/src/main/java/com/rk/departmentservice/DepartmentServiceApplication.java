94e5534576552ecd37cdf2847276d8a998ca3fac
ramakrishna0 <ramakrishnab.1508@gmail.com>
2023-12-25 17:49:13 -0600

    OpenAPI in department-service

--- microservices/springboot-microservices/department-service/src/main/java/com/rk/departmentservice/DepartmentServiceApplication.java
+++ microservices/springboot-microservices/department-service/src/main/java/com/rk/departmentservice/DepartmentServiceApplication.java
@@ -1,10 +1,35 @@
 package com.rk.departmentservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Department Service REST API",
+				description = "Department Service REST APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "RK",
+						email = "rk@gmail.com",
+						url = "http://localhost:8761/eureka/"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "http://localhost:8761/eureka/"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Department-Service Doc",
+				url = "http://localhost:8761/eureka/"
+		)
+)
 @SpringBootApplication
 public class DepartmentServiceApplication {
 
--- microservices/springboot-microservices/department-service/src/main/java/com/rk/departmentservice/controller/DepartmentController.java
+++ microservices/springboot-microservices/department-service/src/main/java/com/rk/departmentservice/controller/DepartmentController.java
@@ -2,6 +2,9 @@ package com.rk.departmentservice.controller;
 
 import com.rk.departmentservice.dto.DepartmentDTO;
 import com.rk.departmentservice.service.DepartmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(
+        name = "Department Service - Department Controller",
+        description = "Department Controller exposes REST APIs for Department-Service"
+)
 @RestController
 @RequestMapping("api/departments")
 @AllArgsConstructor
@@ -19,6 +26,14 @@ public class DepartmentController {
 
     private DepartmentService departmentService;
 
+    @Operation(
+            summary = "Save Department REST API",
+            description = "Save Department REST API is used to save department object in a database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 Created"
+    )
     //build save department REST API
     @PostMapping()
     public ResponseEntity<DepartmentDTO> saveDepartment(@RequestBody DepartmentDTO departmentDTO) {
@@ -26,6 +41,14 @@ public class DepartmentController {
         return new ResponseEntity<>(savedDepartmentDTO, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get Department REST API",
+            description = "Get Department REST API is used to get the department object from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 Success"
+    )
     //build get department by code
     @GetMapping("/{department-code}")
     public ResponseEntity<DepartmentDTO> getDepartment(@PathVariable(name = "department-code") String departmentCode) {
